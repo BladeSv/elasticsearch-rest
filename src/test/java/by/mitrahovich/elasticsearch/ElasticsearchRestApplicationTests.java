503ca9a066e603107f7d464401c097ddf32cb567
Mikhail Mitrakhovich <bladesvet@gmail.com>
2023-09-07 01:46:54 +0500

    task 3 implementation

--- src/test/java/by/mitrahovich/elasticsearch/ElasticsearchRestApplicationTests.java
+++ src/test/java/by/mitrahovich/elasticsearch/ElasticsearchRestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ElasticsearchRest1ApplicationTests {
+class ElasticsearchRestApplicationTests {
 
     @Test
     void contextLoads() {
